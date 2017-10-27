c0eb3770b6c0f7a2f3636d55b6fffa1435c85302
andre.de.melo <andre.de.melo@viverebrasil.com.br>
2017-10-27 12:25:42 -0200

    Commit Inicial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,31 @@
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  beaconData: any;
+
+  constructor(
+    public navCtrl: NavController, 
+    public platform: Platform, 
+    public changeDetectorRef: ChangeDetectorRef) {
+
+  }
+
+  startScanning() {
+    this.platform.ready().then(() => {
+      evothings.eddystone.startScan((data) => {
+        this.beaconData = data;
+        console.log(this.beaconData);
+        setTimeout(() => {
+          this.changeDetectorRef.detectChanges();
+        }, 10000);
+      }, error => console.error(error));
+    });
+  }
+
+}
